3cc25cf49ebaf10a4c2e6dce676ad505c19834df
Alfred Githinji <alfredgithinji87@gmail.com>
2023-10-11 18:27:41 +0300

    Toggled colors and shapes

--- src/containers/Contact.jsx
+++ src/containers/Contact.jsx
@@ -17,7 +17,7 @@ const Contact = () => {
               className="w-72 tablet:w-[550px] px-2 appearance-none h-36 py-2 border-2 text-gray-700 leading-tight focus:outline-none  focus:border-purple-400 placeholder:text-sm bg-slate-200 border-none rounded-lg text-start "
             ></textarea>
           </form>
-          <button className="bg-red-600 hover:bg-red-700 h-12 w-48 rounded-lg text-slate-100">
+          <button className="bg-primary hover:bg-red-700 h-12 w-48 rounded-lg text-slate-100">
             Submit
           </button>
         </section>
